6fd56378d24cbcb5a00e9448aad656b8b6fc0e60
Ryan Rong <ryanrong24@gmail.com>
2025-08-05 19:29:25 -0700

    debug github pages 11

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base:'/gaussian-atlas/',
   server: {
     host: "::",
     port: 8080,
